aec06c214118af48e30f58d8425ddd529355bb75
Jason Yuen <jayuen@gmail.com>
2014-05-02 00:33:57 -0400

    adds schema column to output, removes debugging

--- all_schema.sql
+++ all_schema.sql
@@ -1,6 +1,6 @@
 ﻿CREATE OR REPLACE FUNCTION for_all_schema(raw_query text) RETURNS integer AS $$
 DECLARE 
-  final_statement text;
+final_statement text;
   all_schema_query text;
   schema RECORD;
   schema_names text[];
@@ -16,29 +16,29 @@ BEGIN
   FOR schema IN SELECT schema_name FROM information_schema.schemata WHERE schema_name = 'public' OR schema_name LIKE 'account_%'
   LOOP
     schema_names := array_append(schema_names, CAST(schema.schema_name AS text));
-    all_schema_query := raw_query;
     final_statement := 'CREATE TEMPORARY TABLE ' || schema.schema_name || '_temptable AS '
-	|| 'SELECT q.* FROM ('
-	|| all_schema_query
-	|| ') AS q';
+      || 'SELECT ' 
+      || 'CAST(''' || schema.schema_name || ''' AS text) AS schema_name, '
+      || 'q.* FROM ('
+	    || raw_query
+	    || ') AS q';
     EXECUTE final_statement;
   END LOOP;
 
   FOREACH name IN ARRAY schema_names
   LOOP
     temptable_select := 'SELECT * FROM ' || name || '_temptable';
-    RAISE NOTICE 'statement: %', temptable_select;
     temptable_selects := array_append(temptable_selects, temptable_select);
   END LOOP;
-  final_statement := 'CREATE TEMPORARY TABLE temptable AS ' || array_to_string(temptable_selects, ' UNION ALL ');
+  final_statement := 'CREATE TEMPORARY TABLE temptable AS ' 
+    || array_to_string(temptable_selects, ' UNION ALL ')
+    || ' ORDER BY schema_name';
   EXECUTE final_statement;
   RETURN 0;
 END;$$ LANGUAGE plpgsql;
 
+-- example usage
 select for_all_schema('SELECT * FROM badge_types LIMIT 10'); select * from temptable;
-
---CREATE TEMPORARY TABLE tempy AS SELECT * FROM public_temptable UNION ALL SELECT * FROM public_temptable;
---SELECT * FROM tempy;
 
 -- PERFORM for any statements that don't return anything
 -- can to cast results
